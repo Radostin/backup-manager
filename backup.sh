e1f5e60ba164c0a4298e26ab026ddfed02131d31
Radostin Bonev <radbonev@gmail.com>
2017-12-09 07:22:33 -0500

    save

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source ~/backup-manager/.env
+source .env
 
 for i in "${MYSQL_DATABASES[@]}"
 do
@@ -11,14 +11,14 @@ do
    mysqldump -u ${MYSQL_USER} -p${MYSQL_PASSWORD} ${DATABASE} | gzip > ${TMP_DIR}/${FILE_NAME}
 
    if [ -e ${TMP_DIR}/${FILE_NAME} ]; then
+
     aws s3 cp ${TMP_DIR}/${FILE_NAME} s3://${S3_BUCKET}/${S3_ROOT}/${FILE_NAME}
+
     if [ "$?" -ne "0" ]; then
         echo "Upload to AWS failed"
     fi
+    
     rm ${TMP_DIR}/${FILE_NAME}
 fi
-
-echo "Backup file not created"
-exit 1
 done
 exit;
